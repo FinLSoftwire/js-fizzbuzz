402d122e9f4b77fb28f3fa6dfda3a518b31d99be
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-02 15:41:44 +0100

    Added custom rule definitions in the command line

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,68 +1,118 @@
 const prompt = require("prompt-sync")();
 const { argv } = require('node:process');
 
+class WordOp {
+    constructor(word, op) {
+        this.word = word;
+        this.op = op;
+    }
+    runOp(arr) {
+        return this.op(arr, this.word);
+    }
+}
+const outputAppend = function (arr, w) { return arr.concat([w]); }
+const outputWrite = function (arr, w) { return [w]; }
+const outputReverse = function(arr, w) { return arr.reverse(); }
+const outputInsertB = function (arr, w) {
+    if (arr.length === 0 || arr[0][0] === "B") {
+        return [w].concat(arr);
+    } else {
+        // Take tail of list and insert the word before it but after the head
+        // (workaround for lack of insert)
+        return [arr[0], w].concat(arr.slice(1));
+    }
+}
+
 function toCapitalized(string) {
     return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
 }
+
 // This is our main function
 function fizzbuzz() {
-    // Take command line arguments to decide rules
-    // Allow for either the number or word to be used as a command line argument
-    let rules = [true, true, true, true, true, true];
-    let falseRuleSet = new Set([0,1,2,3,4,5]);
-    let groupings = [["3","Fizz"],["5","Buzz"],["7","Bang"],["11","Bong"],["13","Fezz"],["17"]]
+    // Store an array of numbers that have a rule in use
+    let ruleArray = [];
     const ruleMap = new Map();
+    ruleMap.set(3,new WordOp("Fizz", outputAppend));
+    ruleMap.set(5,new WordOp("Buzz", outputAppend));
+    ruleMap.set(7,new WordOp("Bang", outputAppend));
+    ruleMap.set(11,new WordOp("Bong", outputWrite));
+    ruleMap.set(13,new WordOp("Fezz", outputInsertB));
+    ruleMap.set(17,new WordOp("", outputReverse));
+    let ruleSet = new Set();
+    let groupings = [["3","Fizz"],["5","Buzz"],["7","Bang"],["11","Bong"],["13","Fezz"],["17"]]
+    const ruleIndices = new Map();
     for (let index = 0; index < groupings.length; index++) {
         groupings[index].forEach(function (value) {
-            ruleMap.set(value, index);
+            ruleIndices.set(value, parseInt(groupings[index][0],10));
         });
     }
+    // Take command line arguments to decide rules
+    // Allow for either the number or word to be used as a command line argument
     // Set non-specified rules to false
+    // Specify rules first, then after 'def' specify definitions in format NUM RULE WORD
     if (argv.length > 2) {
-        argv.forEach(function(value, index) {
-            // Ignore the first two arguments
-            const capitalizedValue = toCapitalized(value);
-            if (index > 1 && ruleMap.has(capitalizedValue)) {
-                // If the argument is present, remove it from the false rule set
-                falseRuleSet.delete(ruleMap.get(capitalizedValue));
+        let argIndex = 2;
+        let definingNew = false;
+        if (argv[argIndex] === "def") {
+            ruleMap.forEach(function(operation, integerKey, map) {ruleArray.push(integerKey);});
+        }
+        while (argIndex < argv.length) {
+            let value = argv[argIndex];
+            if (definingNew) {
+                // Extract number, rule, and (if not reversing) word
+                if (argIndex >= argv.length-1)
+                    throw new Error("Rule definition not provided.");
+                let integerValue = parseInt(value,10);
+                if (ruleMap.has(integerValue))
+                    throw new Error("Rule already defined.");
+                let ruleType = argv[argIndex+1].toLowerCase();
+                let word = "";
+                if (ruleType !== "reverse")
+                    word = toCapitalized(argv[argIndex+2]);
+                let outputFunc = null;
+                switch (ruleType) {
+                    case "reverse": outputFunc = outputReverse; argIndex += 2;
+                        break;
+                    case "append": outputFunc = outputAppend; argIndex += 3;
+                        break;
+                    case "write": outputFunc = outputWrite; argIndex += 3;
+                        break;
+                    case "insert": outputFunc = outputInsertB; argIndex += 3;
+                }
+                ruleMap.set(integerValue, new WordOp(word,outputFunc));
+                ruleArray.push(integerValue);
+            } else {
+                // Toggle defining mode
+                if (value === "def") {
+                    definingNew = true;
+                    argIndex++;
+                    continue;
+                }
+                const capitalizedValue = toCapitalized(value);
+                if (ruleIndices.has(capitalizedValue))
+                    ruleArray.push(ruleIndices.get(capitalizedValue));
+                else
+                    throw new Error("Value " + capitalizedValue + " is not recognized.");
+                argIndex++;
             }
-        });
-        falseRuleSet.forEach(function (ruleIndex) {
-            rules[ruleIndex] = false;
-        });
+        }
+    } else {
+        ruleMap.forEach(function(operation, integerKey, map) {ruleArray.push(integerKey);});
     }
+    // Sort the rules in ascending order
+    ruleArray.sort(function (a,b) {return a-b; });
     let maxBoundInput = prompt("Insert a maximum number for FizzBuzz: ");
     let maxBound = 100;
     if (maxBoundInput.length > 0)
         maxBound = parseInt(maxBoundInput,10);
     for (let i = 1; i <= maxBound; i++) {
         let output = [];
-        // All multiples of 3 add Fizz
-        if (rules[0] && i % 3 === 0)
-            output.push("Fizz");
-        // All multiples of 3 add Buzz
-        if (rules[1] && i % 5 === 0)
-            output.push("Buzz");
-        // All multiples of 7 add Bang
-        if (rules[2] && i % 7 === 0)
-            output.push("Bang");
-        // Multiples of 11 override previous rules to only write Bong
-        if (rules[3] && i % 11 === 0)
-            output = ["Bong"]; // Overwrite - not concatenate
-        // Multiples of 13 Insert Fezz before the first word beginning in B
-        if (rules[4] && i % 13 === 0) {
-            // Find the first word beginning with B
-            if (output.length === 0 || output[0][0] === "B") {
-                output = ["Fezz"].concat(output);
-            } else {
-                // Take tail of list and insert Fezz before it but after the head
-                // (workaround for lack of insert)
-                output = [output[0], "Fezz"].concat(output.slice(1));
-            }
-        }
-        // Reverse the list for multiples of 17
-        if (rules[5] && i % 17 === 0)
-            output.reverse();
+        // If there is a rule for the number, then apply it to the output
+        // Note that this means that any higher numbers may overwrite lower ones
+        ruleArray.forEach(function(value, arr) {
+            if (i % value === 0)
+                output = ruleMap.get(value).runOp(output);
+        });
         // If the output is empty (i.e. none of the criteria apply) then set output to the number
         if (output.length === 0)
             output = [i];
