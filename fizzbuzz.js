e74b5fac14ca721ce00d214330c22880af0707d9
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-02 10:19:59 +0100

    Added command line arguments to select rules in use

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,32 +3,49 @@ const { argv } = require('node:process');
 // This is our main function
 function fizzbuzz() {
     // Take command line arguments to decide rules
+    // Allow for either the number or word to be used as a command line argument
     let rules = [true, true, true, true, true, true];
-    if (argv.length > 2) {
-        // Rules are specified so check for them
+    let falseRuleSet = new Set([0,1,2,3,4,5]);
+    let groupings = [["3","Fizz"],["5","Buzz"],["7","Bang"],["11","Bong"],["13","Fezz"],["17"]]
+    const ruleMap = new Map();
+    for (let index = 0; index < groupings.length; index++) {
+        groupings[index].forEach(function (value) {
+            ruleMap.set(value, index);
+        });
     }
-    // Prompt the user for a maximum number
+    // Set non-specified rules to false
+    if (argv.length > 2) {
+        argv.forEach(function(value, index) {
+            // Ignore the first two arguments
+            if (index > 1 && ruleMap.has(value)) {
+                // If the argument is present, remove it from the false rule set
+                falseRuleSet.delete(ruleMap.get(value));
+            }
+        });
+        falseRuleSet.forEach(function (ruleIndex) {
+            rules[ruleIndex] = false;
+        });
+    }
     let maxBoundInput = prompt("Insert a maximum number for FizzBuzz: ");
     let maxBound = 100;
     if (maxBoundInput.length > 0)
         maxBound = parseInt(maxBoundInput,10);
-    // Loop through numbers in range
     for (let i = 1; i <= maxBound; i++) {
         let output = [];
         // All multiples of 3 add Fizz
-        if (i % 3 === 0)
+        if (rules[0] && i % 3 === 0)
             output.push("Fizz");
         // All multiples of 3 add Buzz
-        if (i % 5 === 0)
+        if (rules[1] && i % 5 === 0)
             output.push("Buzz");
         // All multiples of 7 add Bang
-        if (i % 7 === 0)
+        if (rules[2] && i % 7 === 0)
             output.push("Bang");
         // Multiples of 11 override previous rules to only write Bong
-        if (i % 11 === 0)
+        if (rules[3] && i % 11 === 0)
             output = ["Bong"]; // Overwrite - not concatenate
         // Multiples of 13 Insert Fezz before the first word beginning in B
-        if (i % 13 === 0) {
+        if (rules[4] && i % 13 === 0) {
             // Find the first word beginning with B
             if (output.length === 0 || output[0][0] === "B") {
                 output = ["Fezz"].concat(output);
@@ -39,7 +56,7 @@ function fizzbuzz() {
             }
         }
         // Reverse the list for multiples of 17
-        if (i % 17 === 0)
+        if (rules[5] && i % 17 === 0)
             output.reverse();
         // If the output is empty (i.e. none of the criteria apply) then set output to the number
         if (output.length === 0)
