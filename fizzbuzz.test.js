c8619d0b659e679a2365e83c9ae1cb20465881a7
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-03 16:01:15 +0100

    Test cases added for all multiples 3-13 for primary use cases

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -35,8 +35,34 @@ describe('Inserts a word into the output before any word starting in B', () => {
 });
 describe('Runs FizzBuzz with specified command line arguments', () => {
     test('FizzBuzz should output 1 when given an endpoint >=1 with no command line arguments', () => {
-        const consoleSpy = jest.spyOn(console, 'log');
-        fizzbuzz([]);
-        expect(consoleSpy).toHaveBeenCalledWith("1");
+        expect(fizzbuzz([],1, 1)).toEqual("1");
+    });
+    test('FizzBuzz should output Fizz for 3 with no command line arguments', () => {
+        expect(fizzbuzz([],3, 3)).toEqual("Fizz");
+    });
+    test('FizzBuzz should output Buzz for 5 with no command line arguments', () => {
+        expect(fizzbuzz([],5, 5)).toEqual("Buzz");
+    });
+    test('FizzBuzz should output FizzBuzz for 15 with no command line arguments', () => {
+        expect(fizzbuzz([],15, 15)).toEqual("FizzBuzz");
+    });
+    test('FizzBuzz should output Bang for 7 with no command line arguments', () => {
+        expect(fizzbuzz([],7, 7)).toEqual("Bang");
+    });
+    test('FizzBuzz should output FizzBuzzBang for 3*5*7=105 with no command line arguments', () => {
+        expect(fizzbuzz([],105, 105)).toEqual("FizzBuzzBang");
+    });
+    test('FizzBuzz should output Bong for 11 with no command line arguments', () => {
+        expect(fizzbuzz([],11, 11)).toEqual("Bong");
+    });
+    test('FizzBuzz should only output Bong for multiples of 11 (even if divisible by 3, 5, 7) with no command line arguments', () => {
+        expect(fizzbuzz([],33, 33)).toEqual("Bong");
+        expect(fizzbuzz([],55, 55)).toEqual("Bong");
+        expect(fizzbuzz([],77, 77)).toEqual("Bong");
+    });
+    test('FizzBuzz should insert Fezz for multiples of 13 before the first B-prefixed word with no command line arguments', () => {
+        expect(fizzbuzz([],13, 13)).toEqual("Fezz");
+        expect(fizzbuzz([],65, 65)).toEqual("FezzBuzz");
+        expect(fizzbuzz([],195, 195)).toEqual("FizzFezzBuzz");
     });
 });
